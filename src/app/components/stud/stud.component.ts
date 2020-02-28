6346df12dac7f7cb0f215496680624495703d462
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2020-02-28 22:25:24 +0100

    chore: cleanup

--- src/app/components/stud/stud.component.ts
+++ src/app/components/stud/stud.component.ts
@@ -1,18 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Colour, IStud, PieceType } from 'src/app/model';
+import { Component, Input } from '@angular/core';
+import { IStud } from 'src/app/model';
 
 @Component({
   selector: 'lego-stud',
   styleUrls: ['./stud.component.less'],
   templateUrl: './stud.component.html',
 })
-export class StudComponent implements OnInit {
-  @Input() public colour: Colour = Colour.Black;
-  @Input() public pieceType: PieceType = PieceType.Empty;
+export class StudComponent {
   @Input() public stud: IStud;
-
-  public ngOnInit(): void {
-    console.log(this.stud);
-    // PieceType.
-  }
 }
